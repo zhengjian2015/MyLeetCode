4d09a0f6534e08cee8685a57f7cbe40d24a22351
zhengjian <570470242@qq.com>
2023-01-15 02:07:11 +0800

    环形队列

--- src/com/shanggg/queue/CircleArrayQueueDemo.java
+++ src/com/shanggg/queue/CircleArrayQueueDemo.java
@@ -2,14 +2,10 @@ package com.shanggg.queue;
 
 import java.util.Scanner;
 
-/**
- *
- * 这个队列的问题是 空间浪费， 队列取出后，再添加无法添加
- */
-public class ArrayQueueDemo {
+public class CircleArrayQueueDemo {
     public static void main(String[] args) {
         //测试一把
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        CircleArray arrayQueue = new CircleArray(3);
         char key = ' '; //接收用户输入
         Scanner scanner = new Scanner(System.in);
         boolean loop = true;
@@ -57,24 +53,28 @@ public class ArrayQueueDemo {
     }
 }
 
-//使用数组模拟队列
-class ArrayQueue {
+class CircleArray {
+    private int maxSize; //表示数组的最大容量  最大容量比最多有效数据大1，rear有空出一个
+    //front的变量含义调整：front指向队列的第一个元素，也就是说arr[front]
+    //front的初始值 = 0
+    private int front;
+    //rear变量的含义做一个调整:rear指向队列的最后一个元素的后一个位置，因为希望空出一个位置
+    //rear的初始值 = 0
+    private int rear; //队列尾
+    private int[] arr;//该数据用于存储数据，模拟队列
 
-    private int maxSize; //表示数组的最大容量
-    private int front;   //队列头 指向第一个元素的前一个原始 = -1
-    private int rear;    //队列尾 指向最后一个原始
-    private int[] arr;   //该数据用于存放数据，模拟队列
-
-    public ArrayQueue(int arrMaxSize) {
+    public CircleArray(int arrMaxSize) {
         maxSize = arrMaxSize;
         arr = new int[arrMaxSize];
-        front = -1;  //指向队列头部 front指向队列头的前一个位置
-        rear = -1;   //指向队列尾，指向队列尾的数据
+        front = 0;
+        rear = 0;
     }
 
+
     //判断队列是否满
+    //当队列满时 rear总处在front的前一个位置
     public boolean isFull() {
-        return rear == maxSize - 1;
+        return (rear + 1) % maxSize == front;
     }
 
     //判断队列是否为空
@@ -89,19 +89,26 @@ class ArrayQueue {
             System.out.println("队列满");
             return;
         }
-        rear++;
         arr[rear] = n;
+        //将队列后移动，这里必须考虑取模
+        rear = (rear + 1) % maxSize;
+
     }
+
 
     //数据出队列
     public int getQueue() {
+        //判断数据是否为空
         if (isEmpty()) {
             throw new IllegalArgumentException("队列为空");
         }
-        //先进先出  rear是不对的
-        //return arr[rear--];
-        front++;
-        return arr[front];
+        //这里分析出 front 是指向队列的第一个元素
+        //1.先把 front 对应的值保存在临时变量
+        //2.将 front 往后移
+        //3.将临时变量保存
+        int res = arr[front];
+        front = (front + 1) % maxSize;
+        return res;
     }
 
     //显示队列的所有数据
@@ -111,9 +118,20 @@ class ArrayQueue {
             System.out.println("队列空的，没有数据");
             return;
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        for (int i = front; i < front + size(); i++) {
+            System.out.printf("arr[%d]=%d\n", i % maxSize, arr[i % maxSize]);
+
         }
+    }
+
+    //当前有效数据个数
+    public int size() {
+        //rear=1
+        //front=0
+        //maxSize = 3
+        //rear比front是因为只出不进，front跑到rear的后面了
+        // 其实就是 front-rear的绝对值  这样写说明 空间比有效数据大超过1个也可以
+        return (rear + maxSize - front) % maxSize;
     }
 
     //显示队列的头数据
