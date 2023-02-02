4bd162cf7d634ebab47c20675c8138713d54901b
zhengjian <570470242@qq.com>
2023-02-02 19:44:15 +0800

    双向链表的测试

--- src/com/shanggg/linkedlist/DoubleLinkedListDemo.java
+++ src/com/shanggg/linkedlist/DoubleLinkedListDemo.java
@@ -3,7 +3,29 @@ package com.shanggg.linkedlist;
 public class DoubleLinkedListDemo {
 
     public static void main(String[] args) {
+        //测试
+        System.out.println("双向链表的测试");
+        HeroNode2 hero1 = new HeroNode2(1, "宋江", "及时雨");
+        HeroNode2 hero2 = new HeroNode2(2, "卢俊义", "玉麒麟");
+        HeroNode2 hero3 = new HeroNode2(3, "吴用", "智多星");
+        HeroNode2 hero4 = new HeroNode2(4, "林冲", "豹子头");
 
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.add(hero1);
+        doubleLinkedList.add(hero2);
+        doubleLinkedList.add(hero3);
+        doubleLinkedList.add(hero4);
+        doubleLinkedList.list();
+
+        //修改
+        HeroNode2 newHeroNode = new HeroNode2(4,"公孙胜","入云龙");
+        doubleLinkedList.update(newHeroNode);
+        System.out.println("修改后的链表");
+        doubleLinkedList.list();
+
+        System.out.println("删除后的链表");
+        doubleLinkedList.del(3);
+        doubleLinkedList.list();
     }
 
 
