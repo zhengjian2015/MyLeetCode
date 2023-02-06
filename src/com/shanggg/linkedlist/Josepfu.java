17be76ce0b4e833fa73a355a9a5f2d12a6521769
zhengjian <570470242@qq.com>
2023-02-07 00:32:40 +0800

    环形链表

---
+++ src/com/shanggg/linkedlist/Josepfu.java
@@ -0,0 +1,71 @@
+package com.shanggg.linkedlist;
+
+/**
+ * 约瑟夫问题
+ *
+ *
+ *
+ */
+public class Josepfu {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+//创建一个环形的单向链表
+class CircleSingleLinkedList {
+    //创建一个first节点，当前没有编号
+    private Boy first = null;
+
+    //添加小孩节点，构建成一个环形的链表
+    public void addBoy(int nums) {
+        //nums做一个数据校验
+        if (nums < 1) {
+            System.out.println("nums的值不正确");
+            return;
+        }
+        Boy curBoy = null; //辅助指针，帮助构建环形链表
+        //使用for来创建我们的环形链表
+        for (int i = 1; i < nums; i++) {
+            //根据编号，创建小孩节点
+            Boy boy = new Boy(i);
+            //如果是第一个小孩
+            if (i == 1) {
+                first = boy;
+                first.setNext(first);
+                curBoy = first; //让curBoy指向第一个小孩
+            } else {
+                curBoy.setNext(boy);
+                boy.setNext(first);
+                curBoy = boy;
+            }
+        }
+    }
+}
+
+//创建一个Boy类，表示一个节点
+class Boy {
+    private int no;
+    private Boy next; //指向下一个节点
+
+    public Boy(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public Boy getNext() {
+        return next;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public void setNext(Boy next) {
+        this.next = next;
+    }
+}
